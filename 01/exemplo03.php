039ba0f8135d77f03f85c90a7ef517fe43a4cf92
LBorgess <lucas_rancer@hotmail.com>
2023-07-31 21:49:08 -0300

    Fechando a conexão com o banco de dados

---
+++ 01/exemplo03.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Criando uma conexão com o banco de dados
+    $con = mysqli_connect("localhost", "etec", "123456", "db_exemplo");
+
+    if(mysqli_connect_errno()){
+    echo "Erro ao conectar com  base de dados: " . mysqli_connect_error();
+    } else {
+        echo "Conexão Aberta";
+
+        mysqli_close($con);
+    }
+
+?>
