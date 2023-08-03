c3653b64b94eaa529562e97d5caea6119cbb7713
LBorgess <lucas_rancer@hotmail.com>
2023-08-02 21:33:43 -0300

    Inserção dos dados

--- 01/exemplo05.php
+++ 01/exemplo05.php
@@ -1,5 +1,18 @@
 <?php
 
-    
+    $con = mysqli_connect("localhost", "root", "", "dbpw3");
 
+    if(mysqli_connect_errno()){
+        echo "Falha ao conectar com o MySQL " . mysqli_connect_error();
+    }
+
+    echo "Dados inseridos </br>";
+
+    $sql1 = "INSERT INTO tbpessoa VALUES('Ana', 'Souza', 30)";
+    mysqli_query($con, $sql1);
+
+    $sql2 = "INSERT INTO tbpessoa(nome, sobrenome, idade) VALUES('Carlos', 'Costa', 21)";
+    mysqli_query($con, $sql2);
+
+    mysqli_close($con);
 ?>
