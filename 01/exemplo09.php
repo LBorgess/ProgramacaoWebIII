0a883cfacfc7f9de72aeb6c7471167d444eaaa19
LBorgess <lucas_rancer@hotmail.com>
2023-08-05 15:30:47 -0300

    Selecionando os dados da tabela

---
+++ 01/exemplo09.php
@@ -0,0 +1,20 @@
+<?php
+
+    $con = mysqli_connect("localhost", "root", "", "dbpw");
+
+    if(mysqli_connect_errno()){
+        echo "Erro: " . mysqli_connect_error();
+    } else {
+        $sql = "SELECT * FROM pessoa";
+        $resultado = mysqli_query($con, $sql);
+
+        echo "<h2>Pessoas</h2>";
+
+        while($pessoa = mysqli_fetch_array($resultado)){
+            echo $pessoa['nome'] . " " . $pessoa['sobrenome'] . " - ". $pessoa['idade'] . " anos <br/><br/>";
+        }
+
+        mysqli_close($con);
+    }
+
+?>
