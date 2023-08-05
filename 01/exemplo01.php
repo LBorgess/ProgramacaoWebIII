e10e9fbd64c1b93b7208a150390890c67aefa1fb
LBorgess <lucas_rancer@hotmail.com>
2023-08-05 14:46:12 -0300

    Atualizando arquivo exemplo01.php

--- 01/exemplo01.php
+++ 01/exemplo01.php
@@ -1,14 +1,11 @@
 <?php
 
     // Criando uma conexão com o banco de dados
-    $con = mysqli_connect("localhost", "etec", "123456");
+    $con = mysqli_connect("localhost", "root", "");
 
-    // chegando a conexão
-    // 1º Numero do erro
-    // 2º O erro que
-    // Sem parametro
-    if(mysqli_connect_errno()){
-    echo "Erro ao conectar com  base de dados: " . mysqli_connect_error();
+    // Chegando a conexão
+    if (mysqli_connect_errno()) {
+        echo "Erro ao conectar com o banco de dados : " . mysqli_connect_error();
     } else {
         echo "Conexão Aberta";
     }
