850fa8a3141bd3766186016e1d8b0eb00e622b35
LBorgess <lucas_rancer@hotmail.com>
2023-08-05 15:13:24 -0300

    Inserção de registros

--- 01/exemplo07.php
+++ 01/exemplo07.php
@@ -1,21 +1,18 @@
 <?php
 
-    $con = mysqli_connect("localhost", "root", "", "dbpw3");
+    // Criando conexão com o banco de dados
+    $con = mysqli_connect("localhost", "root", "", "dbpw");
 
+    // Verificando a conexão
     if(mysqli_connect_errno()){
-        echo "Falha ao conectar com o MySQL " . mysqli_connect_error();
-    } else {
-        $sql = "SELECT * FROM tbpessoa";
-
-        $resultado = mysqli_query($con, $sql);
-    
-        echo "<h2>Pessoas - Dados</h2>";
-  
-        while($pessoa = mysqli_fetch_array($resultado)){
-            echo $pessoa['nome'] . " " . $pessoa['sobrenome'] . " " . $pessoa['idade']. "<br/>";
-        }
-
-        mysqli_close($con);
+        echo "Falha ao conectar com o MySQL: " . mysqli_connect_error();
     }
 
+    $sql1 = "INSERT INTO pessoa VALUES('Ana', 'Souza', 20)";
+    mysqli_query($con, $sql1);
+
+    $sql2 = "INSERT INTO pessoa(nome, sobrenome, idade) VALUES('Carlos', 'Costa', 21)";
+    mysqli_query($con, $sql2);
+
+    mysqli_close($con);
 ?>
