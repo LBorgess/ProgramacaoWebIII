cbc8bed1609eaa1c457c6b27fa8395634f937227
LBorgess <lucas_rancer@hotmail.com>
2023-08-05 14:48:11 -0300

    Atualizando arquivo exemplo02.php

--- 01/exemplo02.php
+++ 01/exemplo02.php
@@ -1,7 +1,7 @@
 <?php
 
-    // Criando uma conexão com o banco de dados
-    $con = mysqli_connect("localhost", "etec", "123456", "db_exemplo");
+    // Criando uma conexão com o banco de dados db_exemplo
+    $con = mysqli_connect("localhost", "root", "", "db_exemplo");
 
     if(mysqli_connect_errno()){
     echo "Erro ao conectar com  base de dados: " . mysqli_connect_error();
