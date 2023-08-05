f29ff5dad579650ac4bf2fafd958925b5507a8a1
LBorgess <lucas_rancer@hotmail.com>
2023-08-05 15:22:06 -0300

    Exemplo de formulário inserindo dados em uma tabela.

---
+++ 01/exemplo_formulario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário - Inserindo dados em uma tabela</title>
+</head>
+<body>
+    <form action="inserir_dados.php" method="POST">
+        Nome: <input type="text" name="nome"> </p>
+        Sobrenome: <input type="text" name="sobrenome"> </p>
+        Idade: <input type="text" name="idade"> </p>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ 01/inserir_dados.php
@@ -0,0 +1,19 @@
+<?php
+
+    $con = mysqli_connect("localhost", "root", "", "dbpw");
+
+    if(mysqli_connect_errno()){
+        echo "Erro: " . mysqli_connect_error();
+    } else {
+        $sql = "INSERT INTO pessoa VALUES('$_POST[nome]', '$_POST[sobrenome]', $_POST[idade])";
+
+        if(mysqli_query($con, $sql)){
+            echo "Pessoa inserida com sucesso.";
+        } else {
+            echo "Erro ao inserir: " . mysqli_error($con);
+        }
+
+        mysqli_close($con);
+    }
+
+?>
