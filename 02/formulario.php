223853d36b9fa6ae4563f71ac6bcfb8f83a082ea
LBorgess <lucas_rancer@hotmail.com>
2023-08-07 21:58:42 -0300

    Formatação dos dados em uma tabela

--- 02/formulario.php
+++ 02/formulario.php
@@ -2,8 +2,9 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Formulário - Pessoas</title>
+    <title>Cadastro</title>
 </head>
 <body>
     <h2 class="text-center mb-2">Cadastro de pessoas</h2>
