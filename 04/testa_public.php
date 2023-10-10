84f71eb27bdf6ad53748b5c03a5959aeba8b537b
LBorgess <lucas_rancer@hotmail.com>
2023-10-09 21:29:44 -0300

    HTML na página

--- 04/testa_public.php
+++ 04/testa_public.php
@@ -9,7 +9,19 @@ $pedrinho = new Funcionario();
 $pedrinho->nome = 'Pedrinho';
 
 $mariana = new Estagiario();
-$mariana->nome = '<br/> Mariana';
+$mariana->nome = 'Mariana';
+?>
 
-echo $pedrinho->nome;
-echo $mariana->nome;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publico</title>
+</head>
+<body>
+    <p><?=$pedrinho->nome?></p>
+    <p><?=$mariana->nome?></p>
+</body>
+</html>
