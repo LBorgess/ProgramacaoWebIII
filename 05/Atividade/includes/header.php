f59199d9340bbaf02530dd83bfab64360c8e8235
LBorgess <lucas_rancer@hotmail.com>
2023-10-09 22:00:18 -0300

    Atividade de empresa

---
+++ 05/Atividade/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"  media="screen,projection"/>   
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Calculadora de Gorjeta</title>
+</head>
---
+++ 05/Atividade/index.php
@@ -0,0 +1,25 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container">
+    <div class="col">
+        <h3 class="center">Cálculo de gorjeta</h3>
+        <form action="" method="post">
+            <div class="">
+                <label for="valor">Valor da conta:</label>
+                <input type="number" name="valor" id="valor" autocomplete="off" require>
+            </div>
+            <div class="">
+                <select>
+                    <option value="" disabled selected>Choose your option</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                </select>
+                <label>Materialize Select</label>
+            </div>
+    </div>
+    </form>
+</div>
+
+</div>
+<?php include 'includes/footer.php'; ?>
