6e61bb64820510ebe076e18fc5b1fd6883b9fde3
LBorgess <lucas_rancer@hotmail.com>
2023-10-16 21:17:15 -0300

    Inicio do projeto de jogo da memória

---
+++ 05/Memoria/index.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container">
+    <div class="jumbotron">
+        <h3 class="text-center">Jogo da Memória</h3>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
