6a3645272a0f536fa89f4fedd2967568b8273f68
LBorgess <lucas_rancer@hotmail.com>
2023-10-16 21:44:26 -0300

    Adicionando as imagens para o jogo da memória

--- 05/Memoria/index.php
+++ 05/Memoria/index.php
@@ -1,9 +1,16 @@
 <?php include 'includes/header.php'; ?>
 
 <div class="container">
-    <div class="jumbotron">
+    <div class="jumbotron border-bottom border-dark mt-1">
         <h3 class="text-center">Jogo da Memória</h3>
     </div>
+
+    <!-- Apenas um teste -->
+
+    <div class="card">
+        <img src="./img/favicon" alt="controle" class="card-img-top">        
+    </div>
+    
 </div>
 
 <?php include 'includes/footer.php'; ?>
