7edb6b28f50bdb35a82f115927d16485471de619
LBorgess <lucas_rancer@hotmail.com>
2023-10-18 21:15:28 -0300

    animação de flip - inicio

--- 05/Memoria/includes/header.php
+++ 05/Memoria/includes/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"  media="screen,projection"/>4
+    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="./css/style.css"/>
     <title>Jogo da Memória</title>
     <link rel="icon" type="img/ico" href="favicon.ico">
 </head>
