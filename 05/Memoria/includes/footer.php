7e54ac7fa3db77c85d9fd79d12a912d93d9e98d0
LBorgess <lucas_rancer@hotmail.com>
2023-11-03 17:50:23 -0300

    Reset project

--- 05/Memoria/includes/footer.php
+++ 05/Memoria/includes/footer.php
@@ -1,13 +1,3 @@
-<script type="text/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/memoria.js"></script>
-
-<footer class="flex-shrink-0 py-4 mt-5 bg-dark text-white">
-    <div class="container text-center">
-        <small>Copyright &copy; Etecia</small>
-    </div>
-</footer>
 
 </body>
 </html>
--- 05/Memoria/includes/header.php
+++ 05/Memoria/includes/header.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link type="text/css" rel="stylesheet" href="./css/style.css"/>
     <title>Jogo da Memória</title>
     <link rel="icon" type="img/ico" href="favicon.ico">
