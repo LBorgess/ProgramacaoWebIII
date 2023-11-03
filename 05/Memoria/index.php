a8a8e637f5647a6bf6433afa05b8bbfe5860993a
LBorgess <lucas_rancer@hotmail.com>
2023-11-03 19:25:53 -0300

    Conclusão parcial

--- 05/Memoria/index.php
+++ 05/Memoria/index.php
@@ -1,6 +1,7 @@
 <?php include_once './includes/header.php'; ?>
 
 <div class="wrapper">
+    <h3>Jogo da Memória</h3>
     <div class="stats-container">
         <div id="moves-count"></div>
         <div id="time"></div>
@@ -9,6 +10,8 @@
     <button id="stop" class="hide">Parar</button>
 </div>
 <div class="controls-container">
+    <h3>Bem vindo ao jogo da memória</h3>
+    <br>
     <p id="result"></p>
     <button id="start">Começar</button>
 </div>
